22c7f13bcaa9b223e06359c66d8b53c3df54cb68
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-05-19 16:20:12 -0500

    Logic add for calculate and operate

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
 function App() {
+  Calculate({ total: '5', next: 1, operator: '+' }, '=');
   return (
-    <div>
+    <>
       <Display />
       <ButtonPanel />
-    </div>
+    </>
   );
 }
 
---
+++ src/logic/operate.js
@@ -0,0 +1,32 @@
+import Big from 'big.js';
+
+const Operate = (numberOne, numberTwo, operation) => {
+  const [one, two] = [Big(numberOne), Big(numberTwo)];
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = one + two;
+      break;
+    case '-':
+      result = one - two;
+      break;
+    case 'X':
+      result = one * two;
+      break;
+    case '÷':
+      result = one / two;
+      break;
+    case '%':
+      result = two === 0
+        ? one.div(100)
+        : one.div(100).times(two);
+      break;
+    default:
+      result = null;
+  }
+
+  return result.toString();
+};
+
+export default Operate;
